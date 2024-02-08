0bfec974f1c4ab7ef9a90cf09925adff64e9e4d6
Artem <e.artem.v@gmail.com>
2024-02-08 21:27:46 +0300

    modal

--- src/Modals/ImageModal/ImageModal.tsx
+++ src/Modals/ImageModal/ImageModal.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { useClickAway } from '@uidotdev/usehooks';
 import style from './ImageModal.module.scss';
 import { useAppDispatch, useAppSelector } from '../../store/store';
-import { toggleModal } from '../../store/modalSlise';
+import { getImageUrl, toggleModal } from '../../store/modalSlise';
 
 export default function ImageModal() {
+  const { posts } = useAppSelector((state) => state.posts);
   const { inActive, imageUrl } = useAppSelector((state) => state.modal);
   const dispatch = useAppDispatch();
 
@@ -20,12 +21,32 @@ export default function ImageModal() {
     }
   }, [inActive]);
 
+  function nextImage() {
+    const idImage = posts.find((post) => post.image === imageUrl).id + 1;
+    if (Number(idImage) <= posts.length) {
+      const image = posts.find((post) => post.id === idImage)?.image;
+      dispatch(getImageUrl(image));
+    }
+  }
+
+  function previousImage() {
+    const idImage = posts.find((post) => post.image === imageUrl).id - 1;
+    if (Number(idImage) >= 1) {
+      const image = posts.find((post) => post.id === idImage)?.image;
+      dispatch(getImageUrl(image));
+    }
+  }
+
   return (
     <div className={inActive ? style.modal : style.hide}>
       <div ref={refOutside} className={style.containerImage}>
-        <button type='button' onClick={() => {}} className={style.btnLeft} />
+        <button
+          type='button'
+          onClick={previousImage}
+          className={style.btnLeft}
+        />
         <img className={style.image} src={imageUrl} alt='img' />
-        <button type='button' onClick={() => {}} className={style.btnRight} />
+        <button type='button' onClick={nextImage} className={style.btnRight} />
       </div>
     </div>
   );
